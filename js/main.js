4a5c0004ea6ef58fd017d471a0e0ac8041c13e62
Luis Miguel Costa das Chagas <42444184+luismiguelcosta@users.noreply.github.com>
2021-02-12 10:25:51 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Web", "WordPress", "Woocommerce", "Frontend", "Happy", "React", "Positive", "Drupal"];
+const textArray = ["Web", "WordPress", "Woocommerce", "Frontend", "Happy", "React", "Positive", "Remote"];
 const typingDelay = 100;
 const erasingDelay = 100;
 const newTextDelay = 1500; // Delay between current and next text
